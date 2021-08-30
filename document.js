92095b42be8b2cc3ce61537935076c4844c60867
barnabas agbulu <barnabas.agbulu@vaatiacollege.com.ng>
2021-08-30 14:25:13 +0100

    first commit

---
+++ document.js
@@ -0,0 +1,23 @@
+console.log(document)
+let name = "barnabas is a boy"
+let age = 14
+let btn = true
+let btn2 = false
+let ary =[1,2,3,4,"losh"]
+console.log(ary[4])
+name = "barnabas is 14"
+ary.push(false)
+console.log(ary)
+
+let obj={
+    name:"barnabas",
+    age:12,
+    school:"vaatia college",}
+    
+    obj.name="oche"
+    obj.email="barnabasagbulu|@gmail.com"
+    console.log(obj)
+  
+    let btn =   document.getElementById("btn")
+    btn.addEventListener("click",function(greet) )
+
